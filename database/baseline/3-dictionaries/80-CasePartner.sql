36307d590cb752094ac73b6647914faa9b55895c
Alexander Danilov <alexander.a.danilov@gmail.com>
2020-03-16 13:35:15 +0300

    Add database structure for CasePartner microservice.

---
+++ database/baseline/3-dictionaries/80-CasePartner.sql
@@ -0,0 +1,7 @@
+create table if not exists "CasePartner" (
+  uid int references snap_auth_user(uid)
+, partner int references partnertbl(id)
+, unique(uid, partner)
+);
+
+GRANT ALL ON TABLE "CasePartner" TO carma_db_sync;
---
+++ database/patches/1.350.0-CasePartner.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+$PSQL -f baseline/3-dictionaries/80-CasePartner.sql
+
