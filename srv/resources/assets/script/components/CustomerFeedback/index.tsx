9f11f456d314397de3bb913156aa1b2236c79ef4
Max Taldykin <jorpic@gmail.com>
2020-08-19 16:17:16 +0300

    Modal dialog for customer feedback

--- srv/resources/assets/script/components/CustomerFeedback/index.tsx
+++ srv/resources/assets/script/components/CustomerFeedback/index.tsx
@@ -1,4 +1,8 @@
 import {h, render, FunctionalComponent} from "preact"
+import {useState} from "preact/hooks"
+
+import {Modal} from "./Modal"
+import {Selector, Item} from "./Selector"
 
 
 interface Props {
@@ -6,10 +10,59 @@ interface Props {
 }
 
 const CustomerFeedback: FunctionalComponent<Props> = (props) => {
+  const [value, setValue] = useState(null);
+  const [comment, setComment] = useState("");
+  const [readonly, setReadonly] = useState(false);
+
+  const onSave = async () => {
+    setReadonly(true);
+    try {
+      await new Promise((resolve, reject) => {
+        setTimeout(
+          () => reject(new Error("Что-то пошло не так.")),
+          1000);
+      });
+    } finally {
+      setReadonly(false);
+    }
+  };
+
+  const values = [ // FIXME: get those from dictionary
+    {val: 1, label: "🙂 Клиент доволен"},
+    {val: 2, label: "🙁 Клиент не доволен"},
+    {val: 3, label: "🤐 Клиент не отвечает"}
+  ];
+
   return (
-    <div>
-      <div class="modal-backdrop fade active in" onClick={props.onClose}/>
-    </div>);
+    <Modal
+      title="Оценка качества обслуживания"
+      onClose={props.onClose}
+      onSave={onSave}
+      canSave={value && !readonly}
+    >
+      <form>
+        <Selector>
+          { values.map(({val, label}) =>
+            <Item
+              active={value == val}
+              disabled={readonly}
+              onClick={() => setValue(val)}>
+              <h3>{label}</h3>
+            </Item>
+          )}
+        </Selector>
+        <div>
+          <label>Комментарий</label>
+          <textarea
+            class="form-control"
+            rows={6}
+            readOnly={readonly}
+            value={comment}
+            onInput={ev => setComment((ev.target as HTMLInputElement).value)}
+          />
+        </div>
+      </form>
+    </Modal>);
 }
 
 
