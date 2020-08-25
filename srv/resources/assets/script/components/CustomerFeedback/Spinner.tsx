4551c0bac5c0e9e8a9a3f6e291a229a82fc08685
Max Taldykin <jorpic@gmail.com>
2020-08-25 20:52:04 +0300

    Connect customer feedback dialog to backend

---
+++ srv/resources/assets/script/components/CustomerFeedback/Spinner.tsx
@@ -0,0 +1,4 @@
+import {h, FunctionalComponent} from "preact"
+
+export const Spinner: FunctionalComponent<{}> = (_) =>
+  <span class="mr-2 glyphicon glyphicon-refresh glyphicon-refresh-animate"/>;
