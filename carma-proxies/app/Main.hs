098a47dadf1adc9dcee47c15b472ef67f4e958e7
Serguey Zefirov <sergueyz@gmail.com>
2020-09-10 13:51:33 +0300

    Proxies builds, no actual functionality there right now
    Adding functionality now

--- carma-proxies/app/Main.hs
+++ carma-proxies/app/Main.hs
@@ -6,58 +6,41 @@
 --
 -- Copyright (C) ...
 
+{-# LANGUAGE FlexibleContexts, GeneralizedNewtypeDeriving, OverloadedStrings, StandaloneDeriving #-}
+
 module Main (main) where
 
 import qualified Data.Configurator as Conf
 import           Data.String (fromString)
-import qualified Data.Map as M
-import qualified Data.HashMap.Strict as HM
-import           Data.Aeson (toJSON)
+import           Data.Aeson (ToJSON)
 import           Data.Proxy
-import           Data.List (sortBy)
-import           Data.Function (on)
-import           Data.Swagger (Swagger)
 import           Data.Text (Text)
-import           Text.InterpolatedString.QM
-import qualified Data.Time.Format as Time
 
-import           Control.Concurrent.Lifted
-import           Control.Monad
-import           Control.Monad.Logger (runStdoutLoggingT)
-import           Control.Monad.Reader (MonadReader, asks, ReaderT, runReaderT)
-import           Control.Monad.Catch (MonadThrow, MonadCatch, throwM, catchAll)
+import           Control.Monad.Reader (MonadReader, ReaderT, runReaderT)
+import           Control.Monad.Catch (MonadCatch)
 import           Control.Monad.Trans.Control (MonadBaseControl)
 import           Control.Monad.Base (MonadBase)
 import           Control.Monad.IO.Class (MonadIO)
 
-import           System.Directory (makeAbsolute)
-
 import           Servant
-import           Servant.Client hiding (Response)
-import           Servant.Swagger (toSwagger)
 import qualified Network.Wai.Handler.Warp as Warp
-import           Network.HTTP.Client (Manager, newManager)
-import           Network.HTTP.Client.TLS (tlsManagerSettings)
 
 import           Carma.Utils.Operators
-import           Carma.Monad.LoggerBus.MonadLogger
-import           Carma.Monad
 
 
 -- Server routes
 type AppRoutes
   =    -- Search coordinates by search query.
        -- Example: GET /search/ru-RU,ru/foobarbaz
-       "search" :> Capture "lang" Lang
-                :> Capture "query" SearchQuery
-                :> Get '[JSON] [SearchByQueryResponse]
+       "search":> Capture "query" SearchQuery
+                :> Get '[JSON] SearchResponse
 
+{-
   :<|> -- Search addresses by coordinates
        -- Example: GET /reverse-search/ru-RU,ru/52.32,3.45
        "reverse-search" :> Capture "lang" Lang
                         :> Capture "coords" Coords
                         :> Get '[JSON] SearchByCoordsResponse
-{-
   :<|> -- /debug/...
        "debug" :> (    -- GET /debug/cached-queries
                        "cached-queries" :> Get '[JSON] [DebugCachedQuery]
@@ -70,28 +53,18 @@ type AppRoutes
                   )
 -}
 
-type SwaggerHeaders a
-   = Headers '[Header "Access-Control-Allow-Origin" String] a
-
--- Server routes + swagger debug route
-type AppRoutesWithSwagger
-  =    AppRoutes
-  :<|> -- GET /debug/swagger.json
-       "debug" :> "swagger.json" :> Get '[JSON] (SwaggerHeaders Swagger)
-
 
 main :: IO ()
 main = do
-  cfg <- Conf.load [Conf.Required "app.cfg"]
+  cfg <- Conf.load [Conf.Required "proxies.cfg"]
 
   !(port :: Warp.Port) <- Conf.require cfg "port"
   !(host :: String)    <- Conf.lookupDefault "127.0.0.1" cfg "host"
+  !(token :: Text)     <- Conf.require cfg "dadata_token"
 
-  resCache            <- newIORefWithCounter mempty
+  let appCtx = token
 
-  let appCtx = ()
-
-      api = Proxy :: Proxy AppRoutesWithSwagger
+      api = Proxy :: Proxy AppRoutes
 
       app = serve api $ hoistServer api withReader appServer
         where
@@ -113,193 +86,33 @@ appServer
      , MonadBase IO m
      , MonadIO m
      )
-  => ServerT AppRoutesWithSwagger m
+  => ServerT AppRoutes m
 appServer =
   ( search
-    :<|> revSearch
-    :<|> ( debugCachedQueries
-           :<|> debugCachedResponses
-           :<|> debugStatistics
-         )
   )
-  :<|> debugSwaggerAPI
 
 
 -- Server routes handlers
 
-type AppContext = () -- will expand on that later.
+type AppContext = Text -- will expand on that later.
 
 search
   :: ( MonadReader AppContext m
      , MonadCatch m
      )
-  => Lang -> SearchQuery -> m [()]
-search lang query =
-  -- Writing statistics about any failure case
+  => SearchQuery -> m SearchResponse
+search _query = return $ SearchResponse ["nada", "nope", "нет"]
 
-  throwUnexpectedResonse undefined
+newtype SearchQuery = SearchQuery Text
+  deriving (Eq, Ord, Show)
 
-revSearch
-  :: ( MonadReader AppContext m
-     , MonadCatch m
-     )
-  => Lang -> Coords -> m SearchByCoordsResponse
-revSearch lang coords@(Coords lon' lat') =
+instance FromHttpApiData SearchQuery where
+  parseQueryParam = Right . SearchQuery
 
-  throwUnexpectedResponse undefined
+newtype SearchResponse = SearchResponse [Text]
+  deriving (Eq, Ord, Show)
 
-debugCachedQueries
-  :: ( MonadReader AppContext m
-     , MonadLoggerBus m
-     , MonadIORefWithCounter m
-     )
-  => m [DebugCachedQuery]
-debugCachedQueries = do
-  logInfo "Debugging cached queries..."
-
-  (asks responsesCache >>= readIORefWithCounter)
-    <&> -- Ordering by adding to cache time
-        M.assocs ? sortBy (compare `on` snd ? fst) ? foldl reducer []
-
-  where reducer acc (k, (t, _)) =
-          DebugCachedQuery k [qm| {debugFormatTime t} UTC |] : acc
-
-debugCachedResponses
-  :: ( MonadReader AppContext m
-     , MonadLoggerBus m
-     , MonadIORefWithCounter m
-     )
-  => m [DebugCachedResponse]
-debugCachedResponses = do
-  logInfo "Debugging cached responses..."
-
-  (asks responsesCache >>= readIORefWithCounter)
-    <&> -- Ordering by adding to cache time
-        M.assocs ? sortBy (compare `on` snd ? fst) ? foldl reducer []
-
-  where
-    reducer acc (k, (t, response'))
-      = DebugCachedResponse
-      { request_params = k
-      , time           = [qm| {debugFormatTime t} UTC |]
-      , response_type  = requestType response'
-      , response       = rjson
-      } : acc
-      where rjson = case response' of
-                         SearchByQueryResponse'  x -> toJSON x
-                         SearchByCoordsResponse' x -> toJSON x
-
-debugStatistics
-  :: ( MonadReader AppContext m
-     , MonadLoggerBus m
-     , MonadIORefWithCounter m
-     )
-  => m [StatisticsDay]
-debugStatistics = do
-  logInfo "Reading collected statistics..."
-  (asks statisticsData >>= readIORefWithCounter) <&> M.foldlWithKey reducer []
-
-  where
-    reducer acc k v = day k v : acc
-    reqTypes = Nothing : fmap Just [minBound, maxBound] :: [Maybe RequestType]
-
-    day k v
-      = StatisticsDay
-      { julian_day      = JulianDay k
-      , iso_day         = ISODay k
-      , by_request_type =
-          reqTypes <&> \x -> StatisticsByRequestType
-            { request_type = x
-            , statistics   = HM.toList v <&> byReqType x & mconcat
-            }
-      }
-
-    byReqType
-      :: Maybe RequestType
-      -> ((RequestType, StatisticResolve), Integer)
-      -> RequestsStatistics
-    byReqType reqTypeFilter ((reqType, resolve), c) =
-      case reqTypeFilter of
-           Nothing               -> x
-           Just y | y == reqType -> x
-                  | otherwise    -> mempty
-
-      where x = mempty { total_requests = c }
-                <> succeeded resolve c
-                <> failed resolve c
-
-    failed :: StatisticResolve -> Integer -> RequestsStatistics
-    failed RequestIsFailed          c = mempty { total_failed = c }
-    failed (RequestIsSucceeded _)   _ = mempty
-    failed ResponseIsTakenFromCache _ = mempty
-
-    succeeded :: StatisticResolve -> Integer -> RequestsStatistics
-    succeeded RequestIsFailed _ = mempty
-    succeeded (RequestIsSucceeded True) c
-      = mempty
-      { total_succeeded               = c
-      , succeeded_real_requests       = c
-      , succeeded_real_added_to_cache = c
-      }
-    succeeded (RequestIsSucceeded False) c
-      = mempty
-      { total_succeeded                   = c
-      , succeeded_real_requests           = c
-      , succeeded_real_not_added_to_cache = c
-      }
-    succeeded ResponseIsTakenFromCache c
-      = mempty
-      { total_succeeded            = c
-      , succeeded_taken_from_cache = c
-      }
-
--- Allowing cross-origin requests to be able to use online swagger-codegen.
-debugSwaggerAPI
-  :: Applicative m
-  => m (Headers '[Header "Access-Control-Allow-Origin" String] Swagger)
-debugSwaggerAPI = pure $ addHeader "*" $ toSwagger (Proxy :: Proxy AppRoutes)
-
-
--- Client requests to Nominatim
-
-searchByQuery
-  :: Maybe UserAgent
-  -> Maybe NominatimAPIFormat
-  -> Maybe Lang
-  -> Maybe SearchQuery
-  -> ClientM [SearchByQueryResponse]
-
-reverseSearchByCoords
-  :: Maybe UserAgent
-  -> Maybe NominatimAPIFormat
-  -> Maybe Lang
-  -> Maybe NominatimLon
-  -> Maybe NominatimLat
-  -> ClientM SearchByCoordsResponse
-
-(searchByQuery :<|> reverseSearchByCoords)
-  = client (Proxy :: Proxy NominatimAPIRoutes)
-
-
--- Throwing `UnexpectedResponseResultException` with logging this error
-throwUnexpectedResponse
-  :: (MonadThrow m, MonadReader AppContext m, MonadLoggerBus m)
-  => Response -> m a
-throwUnexpectedResponse x = do
-  logError [qm| Unexpected response result: {x}! |]
-  throwM $ UnexpectedResponseResultException x
-
-
-writeFailureToStatistics
-  :: (MonadReader AppContext m, MonadClock m, MonadStatisticsWriter m)
-  => RequestType -> m ()
-writeFailureToStatistics reqType = do
-  utcTime <- getCurrentTime
-  writeStatistics utcTime reqType RequestIsFailed
-
-
-debugFormatTime :: Time.FormatTime t => t -> String
-debugFormatTime = Time.formatTime Time.defaultTimeLocale "%Y-%m-%d %H:%M:%S"
+deriving instance ToJSON SearchResponse
 
 {-
 dadataAbbrevProxy :: AppHandler ()
@@ -310,3 +123,4 @@ dadataAbbrevProxy = logExceptions ("handler/abbrevproxy") $ do
   return ()
 
 -}
+
