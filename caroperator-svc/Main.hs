c87188c6c3249249cac283c100204104aacf2345
Alexander Danilov <alexander.a.danilov@gmail.com>
2020-09-15 16:26:03 +0300

    Add new client format handlers.

--- caroperator-svc/Main.hs
+++ caroperator-svc/Main.hs
@@ -6,15 +6,24 @@ import           Control.Monad (when, unless, void)
 import           Control.Monad.IO.Class (liftIO)
 import           Text.Regex
 
+import           Data.Char (isDigit)
 import           Data.Default.Class (Default (def))
 import           Data.Maybe (isJust)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as L
-import           Data.Time (Day, defaultTimeLocale, parseTimeM)
+import           Data.Time ( Day
+                           , ParseTime
+                           , defaultTimeLocale
+                           , getCurrentTime
+                           , parseTimeM
+                           , toGregorian
+                           , utctDay
+                           )
 import qualified Data.Configurator as Config
 import qualified Data.Aeson as Aeson
+import           Data.Aeson ((.=))
 import           Data.Pool (Pool, createPool, withResource)
 import qualified Database.PostgreSQL.Simple as PG
 import           Database.PostgreSQL.Simple.SqlQQ (sql)
@@ -53,6 +62,7 @@ main = do
         pgUser    <- Config.require conf "pg.user"
         pgPwd     <- Config.require conf "pg.pass"
         pgDb      <- Config.require conf "pg.db"
+        ltProgram <- Config.require conf "lt.program"
 
         withCStringLen ("Connecting to Postgres on " ++ pgHost) $
           syslog (Just User) Info
@@ -75,6 +85,7 @@ main = do
         scotty httpPort
           $ httpServer pgPool reqLogger
           $ SrvConfig carmaPrg carmaUsr carmaTest
+                      ltProgram
 
     _ -> error $ "Usage: " ++ prog ++ " <config.conf>"
 
@@ -83,14 +94,44 @@ data SrvConfig = SrvConfig
   { cfgSubProgram :: Int
   , cfgCommitter  :: Int
   , cfgTestMode   :: Bool
+  , cfgLTProgram  :: Int
   }
+
+
+vinRx :: Regex
+vinRx = mkRegexWithOpts "^[0-9A-HJ-NPR-Z]{17}$" False True
+
+
+vinOk :: Text -> Bool
+vinOk = isJust . matchRegex vinRx . T.unpack
+
+
+phoneOk :: Text -> Bool
+phoneOk phone = prefix `elem` ["+7"] &&
+                T.length number == 10 &&
+                T.all isDigit number
+    where (prefix, number) = T.splitAt 2 phone
+
+
+parseDate :: ParseTime a => Text -> L.Text -> ActionM a
+parseDate date name =
+    maybe (raise $ L.concat ["Invalid ", name, " format. Should be YYYY-MM-DD."]) pure
+      $ parseTimeM True defaultTimeLocale "%F"
+      $ T.unpack date
 
 httpServer :: Pool PG.Connection -> Middleware -> SrvConfig -> ScottyM ()
 httpServer pgPool reqLogger cfg = do
   middleware reqLogger
 
-  let queryJSON q = do
-        [[res]] <- liftIO $ withResource pgPool $ \c -> PG.query_ c q
+  let query q = liftIO $ withResource pgPool $ \c -> PG.query_ c q
+      queryParam q p = liftIO $ withResource pgPool $ \c -> PG.query c q p
+
+      queryJSON q = do
+        [[res]] <- query q
+        json (res :: Aeson.Value)
+
+      queryParamJSON q p = do
+        [[res]] <- queryParam q p
         json (res :: Aeson.Value)
 
   get "/CarMake" $ queryJSON
@@ -133,9 +174,8 @@ httpServer pgPool reqLogger cfg = do
     sellDate <- param "sellDate" :: ActionM Text
     dealer   <- param "dealer"   :: ActionM Int
 
-    let vinRx = mkRegexWithOpts "^[0-9A-HJ-NPR-Z]{17}$" False True
-    let vinOk = isJust $ matchRegex vinRx $ T.unpack vin
-    unless vinOk $ raise "Invalid VIN format."
+    unless (vinOk vin) $ raise "Invalid VIN format."
+    -- TODO: implement vin checksum
     unless True  $ raise "Invalid VIN checksum."
     when (carMake /= 28) $ raise "Invalid carMake."
 
@@ -186,3 +226,132 @@ httpServer pgPool reqLogger cfg = do
       )
 
     text "Ok"
+
+  -- partner LT --
+
+  get "/LT/subProgram" $ queryParamJSON
+    [sql| with r as (
+            select id, label
+              from "SubProgram"
+             where parent=?
+          )
+          select array_to_json(array_agg(row_to_json(r.*)))
+            from r;
+    |] $ PG.Only $ cfgLTProgram cfg
+
+  get "/LT/carMake" $ queryJSON
+    [sql| with r as (select id, label from "CarMake")
+          select array_to_json(array_agg(row_to_json(r.*)))
+            from r
+    |]
+
+  get "/LT/carModel/:carMake" $ handleErrors $ do
+    carMake <- param "carMake" :: ActionM Int
+    queryParamJSON [sql| with r as (
+                           select id, label, parent as "carMake"
+                             from "CarModel"
+                            where parent = ?
+                         )
+                         select array_to_json(array_agg(row_to_json(r.*)))
+                           from r
+                   |] $ PG.Only carMake
+
+  post "/LT/contract" $ handleErrors $ do
+    let getCurrentYear = liftIO $ ((\(y,_,_) -> (fromInteger y) ::Int) .
+                                   toGregorian .
+                                   utctDay
+                                  ) <$> getCurrentTime
+        yearOk year thisYear = year >= thisYear - 100 && year <= thisYear
+
+    subProgram <-             param "subProgram" :: ActionM Int
+    name       <- T.strip <$> param "name"       :: ActionM Text
+    phone      <- T.strip <$> param "phone"      :: ActionM Text
+    vin        <- T.strip <$> param "vin"        :: ActionM Text
+    cardNumber <- T.strip <$> param "cardNumber" :: ActionM Text
+    carMake    <-             param "carMake"    :: ActionM Int
+    carModel   <- T.strip <$> param "carModel"   :: ActionM Text
+    makeYear   <-             param "makeYear"   :: ActionM Int
+    buyDate    <- T.strip <$> param "buyDate"    :: ActionM Text
+    validUntil <- T.strip <$> param "validUntil" :: ActionM Text
+
+    [[subProgramOk]] <- queryParam
+      [sql| select exists (
+              select id from "SubProgram" where parent = ? and id = ?
+            )
+      |] (cfgLTProgram cfg, subProgram)
+
+    unless subProgramOk $ raise "Invalid subProgram"
+    when (T.null $ T.strip name) $ raise "Invalid name"
+    unless (phoneOk phone) $ raise "Invalid phone"
+    unless (vinOk vin) $ raise "Invalid vin format"
+    realCardNumber <- do
+      if T.null cardNumber
+      then return Nothing
+      else case reads (T.unpack cardNumber) :: [(Int, String)] of
+             [(cn, "")] -> if cn > 0
+                          then return $ Just cn
+                          else raise "Invalid cardNumber"
+             _ -> raise "Invalid cardNumber"
+
+    [[makeOk]] <- queryParam
+      [sql| select exists (
+              select id from "CarMake" where id = ?
+            )
+      |] $ PG.Only carMake
+    unless makeOk $ raise "Invalid carMake"
+
+    thisYear <- getCurrentYear
+    unless (yearOk makeYear thisYear) $ raise "Invalid makeYear"
+
+    (parsedBuyDate :: Day) <- parseDate buyDate "buyDate"
+    (parsedValidUntil :: Day) <- parseDate validUntil "validUntil"
+
+    r <- queryParam
+      [sql| insert into "Contract"
+              ( subprogram, name, phone, vin
+              , cardNumber, make, extra
+              , makeYear, validSince, validUntil
+              , dixi, committer
+              )
+              with p as
+                (select ? :: int     as subprogram
+                      , ? :: text    as name
+                      , ? :: text    as phone
+                      , ? :: text    as vin
+                      , ? :: text    as cardnumber
+                      , ? :: int     as make
+                      , ? :: json    as extra
+                      , ? :: int     as makeyear
+                      , ? :: date    as validsince
+                      , ? :: date    as validuntil
+                      , ? :: boolean as testmode
+                      , ? :: int     as committer
+                )
+              select p.subprogram, p.name, p.phone
+                   , p.vin, p.cardnumber, p.make, p.extra
+                   , p.makeyear, p.validsince, p.validuntil
+                   , p.testmode, p.committer
+                from p
+               where not exists (
+                 select id from "Contract"
+                  where subprogram = p.subprogram
+                    and name       = p.name
+                    and phone      = p.phone
+                    and vin        = p.vin
+                    and cardnumber = p.cardnumber
+                    and make       = p.make
+                    and extra::json::text = p.extra::json::text
+                    and makeyear   = p.makeyear
+                    and validsince = p.validsince
+                    and validuntil = p.validuntil
+                 )
+              returning id
+      |] ( subProgram, name, phone, vin, realCardNumber
+         , carMake, Aeson.object ["carModel" .= carModel], makeYear
+         , parsedBuyDate, parsedValidUntil
+         , not $ cfgTestMode cfg, cfgCommitter cfg
+         )
+
+    case r of
+      [[contractId :: Int]] -> json contractId
+      _ -> raise "Duplicate"
