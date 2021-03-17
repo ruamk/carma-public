416cae12040b27a648640de86a4e4b507905573e
Sergey Timoshenko <sheunhahahaed@gmail.com>
2021-03-17 16:20:07 +0300

    move onclick for uploading

--- carma-casepartner-fe/src/Pages/ShowService.elm
+++ carma-casepartner-fe/src/Pages/ShowService.elm
@@ -2652,7 +2652,6 @@ viewPhotosAccordion model =
                 , SvgAttributes.class "bi bi-plus-square"
                 , SvgAttributes.viewBox "0 0 16 16"
                 , SvgAttributes.style "margin-left: 5px"
-                , SvgEvents.onClick (UploadPhotosClick photoType)
                 ]
                 [ Svg.path
                     [ SvgAttributes.d "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
@@ -2666,7 +2665,7 @@ viewPhotosAccordion model =
 
         viewBefore : List Photo -> Html Msg
         viewBefore photos =
-            div []
+            div [ onClick (UploadPhotosClick "before") ]
                 [ h3 [] [ text "Фото до начала заявки", viewAddPhoto "before" ]
                 , Grid.row []
                     (viewPhotos <| filterByPhotoType "before" photos)
@@ -2674,7 +2673,7 @@ viewPhotosAccordion model =
 
         viewAfter : List Photo -> Html Msg
         viewAfter photos =
-            div []
+            div [ onClick (UploadPhotosClick "after") ]
                 [ h3 [] [ text "Фото после выполнения заявки", viewAddPhoto "after" ]
                 , Grid.row []
                     (viewPhotos <| filterByPhotoType "after" photos)
@@ -2682,7 +2681,7 @@ viewPhotosAccordion model =
 
         viewDifficult : List Photo -> Html Msg
         viewDifficult photos =
-            div []
+            div [ onClick (UploadPhotosClick "difficult") ]
                 [ h3 [] [ text "Фото сложностей", viewAddPhoto "difficult" ]
                 , Grid.row []
                     (viewPhotos <| filterByPhotoType "difficult" photos)
@@ -2690,7 +2689,7 @@ viewPhotosAccordion model =
 
         viewOrder : List Photo -> Html Msg
         viewOrder photos =
-            div []
+            div [ onClick (UploadPhotosClick "order") ]
                 [ h3 [] [ text "Заказ-наряд", viewAddPhoto "order" ]
                 , Grid.row []
                     (viewPhotos <| filterByPhotoType "order" photos)
