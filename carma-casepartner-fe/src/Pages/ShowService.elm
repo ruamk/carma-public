c9c790b972a52fdf727a6cf96a56304e4e16b4f7
Sergey Timoshenko <sheunhahahaed@gmail.com>
2021-03-18 16:19:36 +0300

    use svg from file

--- carma-casepartner-fe/src/Pages/ShowService.elm
+++ carma-casepartner-fe/src/Pages/ShowService.elm
@@ -2646,23 +2646,7 @@ viewPhotosAccordion model =
             List.filter (\photo -> photo.photoType == photoType)
 
         viewAddPhoto =
-            Svg.svg
-                [ SvgAttributes.width "16"
-                , SvgAttributes.height "16"
-                , SvgAttributes.fill "currentColor"
-                , SvgAttributes.class "bi bi-plus-square"
-                , SvgAttributes.viewBox "0 0 16 16"
-                , SvgAttributes.style "margin-left: 5px"
-                ]
-                [ Svg.path
-                    [ SvgAttributes.d "M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-                    ]
-                    []
-                , Svg.path
-                    [ SvgAttributes.d "M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-                    ]
-                    []
-                ]
+            img [ A.src (Api.staticURL "/plus-square.svg") ] []
 
         viewBefore : List Photo -> Html Msg
         viewBefore photos =
