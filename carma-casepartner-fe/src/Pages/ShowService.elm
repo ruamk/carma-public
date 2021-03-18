a4685e931ab8630a6e9d641e7b9ea1d2d77f7bea
Sergey Timoshenko <sheunhahahaed@gmail.com>
2021-03-18 16:10:08 +0300

    remove svg imports

--- carma-casepartner-fe/src/Pages/ShowService.elm
+++ carma-casepartner-fe/src/Pages/ShowService.elm
@@ -58,8 +58,6 @@ import List
 import Maybe exposing (withDefault)
 import MessageToast exposing (MessageToast)
 import Page exposing (Document, Page)
-import Svg
-import Svg.Attributes as SvgAttributes
 import Time
 import Tuple
 import Types as Types
