5b0aa0ff19c6f955ab364357e69dda77209edf6a
Sergey Timoshenko <sheunhahahaed@gmail.com>
2021-04-26 09:47:05 +0300

    fix empty service description declaration on the site

--- carma-casepartner-fe/src/Types.elm
+++ carma-casepartner-fe/src/Types.elm
@@ -122,6 +122,7 @@ emptyServiceDescription =
     , client = ""
     , clientPhone = ""
     , firstAddress = ""
+    , firstAddressComment = ""
     , lastAddress = ""
     , expectedServiceStart = Nothing
     , factServiceStart = Nothing
